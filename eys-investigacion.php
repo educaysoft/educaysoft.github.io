3e0d5c2b603cf1c448716b42785f250cc159ecc6
Stalin Francis <educaysoft@gmail.com>
2021-08-12 09:50:57 -0500

    Subi todos los articulo

---
+++ eys-investigacion.php
@@ -0,0 +1,71 @@
+<?php
+include("head.php");
+?>
+
+<?php
+include("marco-right.php");
+?>
+  
+ <div style="float:left; width:60%; margin:80px auto; padding:1vw">
+   <div  style="width: 100%;">     
+    <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
+       <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center;">
+        	<p style="text-align: center;"  >La investigación cualidad importante para la asesoría tecnológica</p>
+       		 <hr>
+                <p  style="text-align:left;  font-size:0.vw; font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;"   >Posteado por: <span>Stalin Francis</span> el <span> 07-agosto-2021 </span> </p>
+
+        </div>
+    </div>
+  </div>
+  <div style="width: 100%; height:100%;" >
+    <div >
+        <div  style="text-align:justify; font-size: 20px;">
+                <p class="eys-parrafo">En el artículo  <a href="eys-asesoria-tecnica.php"> el futuro de la asesoría técnica en la tecnología de la información </a> se dejo a entender que la tendencía es que la asesorías solo van a ser necesarias para problemas complejos de resolver o que requieran recursos especializados, significando esto que los profesionales deberan estar en continua actualización de sus conocimiento y habilidades a fin de estar preparados para resolver necesidades que el cliente no halla podido resolver con la ayuda de asistentes inteligentes o simplemente del internet.</p> 
+
+<p class="eys-parrafo">En este punto la especialización juego un papel importante, pues la única forma de lograr conocimiento y habilidades de alto nivel es por medio de formación científica que solo obtienen al alcanzar un titulo universitario y de preferencia de 4to nivel o superior.</p>
+
+
+<p class="eys-parrafo"> Una de las muchas opciones es la <a  href="eys-maestria-ti.php"> Maestría en Tecnología de la información </a> que brinda la Universidad Técnica Luis Vargas Torres,  así como las maestría que brinda otras universidades tanto locales como internacionales. </p>
+
+
+               
+
+                 
+                
+<hr>
+            <div style=" bottom: 0; margin-bottom: 0;   width:100%; display: inline-block;" >
+               <div style="width: 40%;  float: left;">
+                <a href="#">&#171;   Previo</a>
+               </div>
+
+               <div style="width: 40%; float: right; text-align: right;">
+                <a href="apendiendo-cpp">Programa para aprender C++ &#187;</a>
+               </div>
+
+              </div>
+        </div>
+    </div>
+  </div> 
+
+  </div>
+
+
+<?php
+include("marco-left.php")
+?>
+ 
+<script >  
+
+ document.title="Softskill"; 
+ 
+ document.getElementById("blog-indice").innerHTML="<p>Contenido</p> <p><a href='#'>inicio</a></p>";
+
+ document.getElementById("blog-relacionados").innerHTML="<p>Relacionados</p> <p><a href='eys-softskill.php'>Softskill</a></p> <p><a href='eys-maestria-ti.php'>Maestría en Tecnología de la Información </a></p> ";
+ </script>
+ 
+
+ 
+
+<?php
+include("foot.php")
+?>
