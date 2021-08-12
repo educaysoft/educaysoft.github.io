7957a893e94fa0dc05782d4f011db382470e0537
Stalin Francis <educaysoft@gmail.com>
2021-08-12 11:13:33 -0500

    subio los marcos

---
+++ marco-left.php
@@ -0,0 +1,10 @@
+  <div style="float:left; margin-top: 82px;">
+    <div id="blog-relacionados" style=" height: 100vh;  border-left: 1px solid black; padding: 1vw;"> 
+        <p>Temas Relacionados</p>
+        <a href="eys-softskill.php">Softskill</a>
+   </div>
+  </div>
+
+</div>  
+
+
